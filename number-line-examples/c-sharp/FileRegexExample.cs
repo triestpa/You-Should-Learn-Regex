133aeb38da7a2f5bbcfe74b27e1216dd1b547714
triestpa <patrick.triest@gmail.com>
2017-10-08 13:08:55 +0700

    Add c# example

---
+++ number-line-examples/c-sharp/FileRegexExample.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace RegexExample
+{
+    class FileRegexExample
+    {
+        static void Main()
+        {
+            string text = File.ReadAllText(@"../test.txt", Encoding.UTF8);
+            Console.WriteLine(text);
+            Regex regex = new Regex("^[0-9]+$", RegexOptions.Multiline);
+            MatchCollection mc = regex.Matches(text);
+            var matches = mc.OfType<Match>().Select(m => m.Value).ToArray();
+            Console.WriteLine(string.Join(" ", matches));
+        }
+    }
+}
